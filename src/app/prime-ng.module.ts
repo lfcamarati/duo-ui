d096cdd64e1a63579225920af440ad755602451d
Lucas Câmara <lfcamara.ti@gmail.com>
2022-10-13 15:30:10 -0300

    update clients view page

--- src/app/prime-ng.module.ts
+++ src/app/prime-ng.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
 
 const modules = [
   CommonModule,
   ButtonModule,
   CardModule,
   TableModule,
+  InputTextModule,
 ]
 
 @NgModule({
